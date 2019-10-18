5f5a45cbbeb9088d41c4cfcec511966e3ecde719
thamerbelfkihthamer <thamer.belfkih@gmail.com>
2019-10-18 02:26:37 +0100

    fix https url

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 export const PATH_BASE = 'https://api.tvmaze.com/search/shows';
 export const PARAM_SEARCH = '?q=';
 export const DEFAULT_QUERY = 'test';
-export const PATH_ITEM = 'http://api.tvmaze.com/shows';
+export const PATH_ITEM = 'https://api.tvmaze.com/shows';
